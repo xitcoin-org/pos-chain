393d1de32973e87e1aed1c0b851a3505bf141a6a
Thomas Cabrit <124535458+Thomas-Cabrit@users.noreply.github.com>
2026-08-18 17:08:31 +0000

    testnet: align final network identity and validator policy

--- x/validatoradmission/types/policy.go
+++ x/validatoradmission/types/policy.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	DefaultMaxApprovedValidators uint32 = 208
-	DefaultMinimumSelfDelegation        = "5000000000000000000000000axtc"
+	DefaultMaxApprovedValidators uint32 = 258
+	DefaultMinimumSelfDelegation        = "1000000000000000000000000axtc"
 )
 
 func ValidatePolicy(maxApprovedValidators uint32, minimumSelfDelegation string) error {
